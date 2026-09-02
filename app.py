2e55c837b07a367b81e9272ec9c874fe2efe32ee
JesuRosales70 <23040166@itdurango.edu.mx>
2026-09-01 18:21:20 -0600

    Migracion a MySQL local configurada exitosamente

--- app.py
+++ app.py
@@ -104,6 +104,8 @@ if __name__ == "__main__":
     try:
         crear_base_datos()
     except Exception as e:
-        print(f"Aviso BD: Recuerda crear primero la base de datos en MySQL. Detalle: {e}")
+        print(f"Aviso BD: {e}")
 
-    app.run(debug=True)
+    # Render define la variable PORT de forma dinámica
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
